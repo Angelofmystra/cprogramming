f9a79bdd0ad8e66f92fa5eedb5121699a42d5f44
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-08-05 17:13:28 +0100

    Initial array created, and can now print afterwards.

--- 16.c
+++ 16.c
@@ -11,20 +11,20 @@ int choose_pivot(int A[], int left, int right)
 int swap(int A[], int a, int b)
 {
     int temp = A[a];
-    array[a] = array[b];
-    array[b] = temp;
+    A[a] = A[b];
+    A[b] = temp;
     return 0;
 }
 
 int partition(int A[], int left, int right)
 {
     int pivot_index = choose_pivot(A, left, right);
-    int pivot_value = array[pivot_index];
+    int pivot_value = A[pivot_index];
     swap(A, pivot_index, right);
     int store_index = left;
     for(int i=left; i <= right; i++)
     {
-        if(array[i] <= pivot_value)
+        if(A[i] <= pivot_value)
         {
             swap(A, i, store_index);
             store_index = store_index + 1;
@@ -37,7 +37,7 @@ int partition(int A[], int left, int right)
 /*
  *    A[] is the array to be sorted. i is the starting value, usually 1. k is the length of the array to be sorted.
  */
-int quicksort(int A[], i, k)
+int quicksort(int A[], int i, int k)
 {
     if(i < k)
     {
@@ -45,15 +45,25 @@ int quicksort(int A[], i, k)
         quicksort(A, i, p-1);
         quicksort(A, p+1, k);
     }
+    return 0;
 }
 
 int main()
 {
     int size = 20;
     int array[size];
-    // assign stuff to the array
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = rand() % 50;
+    }
+
     quicksort(array, 0, size-1);
-    // print array to see if sorted
+
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d\n", array[i]);
+    }
     return 0;
 }
 
