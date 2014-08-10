d756e680918a808e9847590a0b863b270f35c3f0
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-08-10 22:27:20 +0100

    A little more broken

--- 28.c
+++ 28.c
@@ -11,18 +11,38 @@ void forLoop(int A[], int n, int (*func)())
     for(int i = 0; i < n; i++)
         A[i] = func(i);        
 }
-
-void forLoop2(int A[], int n, int (*func)(int))
+/*
+ * It requires as a parameter a function that takes an int and returns an int.
+ */
+/*void forLoop2(int A[], int n, int (*func)(int x))
 {
     for(int i = 0; i < n; i++)
-        A[i] = func();
-}
+        A[i] = func(x);
+}*/
 
 void forLoop3(int A[], int n, int (*func)(), int param)
 {
     for(int i = 0; i < n; i++)
         A[i] = func(param);
 }
+
+void forLoop5(void *datastructure, int (*func)(const int *), const int *x)
+{
+    while(*datastructure->next != NULL)
+        *datastructure = func(x);
+        *datastructure
+}    
+/*void forLoop4(int A[], int n, int (*func)(), ...)
+{
+    va_list args;
+    va_start(args, A[], n, func);
+    for(int i = 0; i < n; i++)
+        
+    va_end(args);
+
+    //
+}*/
+
 /*
  * Apply the function to each element in the array. Make sure the types line up.
  */
@@ -43,9 +63,9 @@ int main()
     int size = 10;
     int array[size];
     forLoop(array, size, multiply);
-    forLoop2(array, size, multiply(size));
+//    forLoop2(array, size, multiply(size));
     forLoop3(array, size, multiply, 3);
-    GenericForLoop(array, size, multiply);
+    GenericForLoop((int)array, size, multiply);
     GenericDatastructureForLoop(array, size, multiply); // yes function should work on arrays
     // create datastructure
     // GenericDatastructureForLoop(&datastructure, size, multiply);
