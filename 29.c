b671e26f4f76d265b9a2f12bd533f05d37add13f
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-08-10 18:11:14 +0100

    Euclid function

---
+++ 29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int euclid( int m, int n ) {
+    if( n>0 ) {
+        return euclid( n, m%n );
+    } else {
+        return m;
+    }
+}
+
+int main( void ) {
+    printf( "%d\n", euclid( 14, 12 ) ) ;
+    printf( "%d\n", euclid( 14, 11 ) ) ;
+    printf( "%d\n", euclid( 558, 198 ) ) ;
+    return 0 ;
+}
